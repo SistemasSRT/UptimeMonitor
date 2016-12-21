cdf60265410fbb0eb5588c53d89bc114f1d9e089
Guillermo Tala <guille86598@gmail.com>
2016-12-21 15:31:21 -0300

    limpieza homecontrolller

--- Server/Controllers/HomeController.cs
+++ Server/Controllers/HomeController.cs
@@ -14,54 +14,6 @@ namespace Server.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
-
-            try
-            {
-                var pingSender = new Ping();
-                PingOptions options = new PingOptions();
-
-                // Use the default Ttl value which is 128,
-                // but change the fragmentation behavior.
-                options.DontFragment = true;
-
-                var monitor = new Model.Monitor();
-                using(Model.Context db = new Model.Context())
-                {
-                    monitor = db.Monitors.First(x => x.Id == 5);
-                }
-
-                // Create a buffer of 32 bytes of data to be transmitted.
-                string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-                byte[] buffer = Encoding.ASCII.GetBytes(data);
-                int timeout = 120;
-                PingReply reply = pingSender.Send(monitor.IP, timeout, buffer, options);
-                MonitorLog monitorLog = new MonitorLog();
-                monitorLog.Resultado = reply.RoundtripTime.ToString();
-                monitorLog.Fecha = DateTime.Now;
-                monitorLog.Monitor = monitor;
-
-                if (reply.Status == IPStatus.Success)
-                {
-                    monitorLog.TipoResultado = TipoResultado.Correcto;
-                }
-                else
-                {
-                    monitorLog.TipoResultado = TipoResultado.Error;
-                    monitorLog.Resultado = monitorLog.Resultado + " - Ip status: " + reply.Status.ToString();
-                }
-
-                using (Model.Context db = new Model.Context())
-                {
-                    db.MonitorLogs.Add(monitorLog);
-                    db.SaveChanges();
-                }
-            }
-            catch (Exception ex)
-            {
-                throw;
-            }
-
             return View();
         }
     }
