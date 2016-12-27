f9c8018f70e723070f7b479e55de6993c615dd57
margaale <margaale@gmail.com>
2016-12-27 17:12:28 -0300

    interceptor redirecciona a login si da 401

--- Server/Scripts/Interceptores/tokenInterceptor.factory.js
+++ Server/Scripts/Interceptores/tokenInterceptor.factory.js
@@ -10,11 +10,16 @@
     function tokenInterceptor(authorization) {
         var service = {
             request: function (request) {
-                request.headers.Authorization = 'Bearer ' + authorization.getToken();                
+                request.headers.Authorization = 'Bearer ' + authorization.getToken();
                 return request;
             },
-            response: function (response) {
-                console.log(response);
+            response: function (response) {                
+                return response;
+            },
+            responseError: function (response) {
+                if (response.status == 401) {
+                    window.location.href = 'http://localhost:55749/Login?client_id=9cae8e3f-1961-41f4-92e9-44b9c283474b&redirect_uri=http://localhost:51388/Token';
+                }
                 return response;
             }
         };
